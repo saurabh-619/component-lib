d38510cc4355c7db74b9d4d904077c27d312d45d
saurabh-619 <saurabhbomble107@gmail.com>
2021-07-13 21:31:29 +0530

    Second comit

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { Button } from "../buttons/Button";
+
+export default {
+  title: "Example/Button",
+  component: Button,
+} as Meta;
+
+const Template: Story = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
